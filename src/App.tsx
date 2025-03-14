d4c200d21b79693a6b40993755291156b6d4cfa2
jordanbenzacken <jbzkjbzk@gmail.com>
2025-03-14 01:46:48 +0100

    ci: free netlify

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
         <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6756564816358571"
-          crossorigin="anonymous"
+          crossOrigin="anonymous"
         ></script>
         <script type="application/ld+json">
           {JSON.stringify({
