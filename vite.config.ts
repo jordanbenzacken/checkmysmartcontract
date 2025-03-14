14ae6c03bf5b057a508484c5da74bf9a08838c43
jordanbenzacken <jbzkjbzk@gmail.com>
2025-03-14 01:18:03 +0100

    ci: env test

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  test: {
-    env: {
-      VITE_SUPABASE_URL: "https://your-supabase-url.com",
-      VITE_SUPABASE_ANON_KEY: "your-supabase-anon-key",
-    },
-  },
   plugins: [react()],
   optimizeDeps: {
     exclude: ["lucide-react"],
